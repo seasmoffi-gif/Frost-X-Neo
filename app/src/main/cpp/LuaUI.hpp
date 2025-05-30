edd300c7350865aa33baeafec9976105e0305364
EpicModder433 <noahjohn0707@icloud.com>
2025-05-30 21:51:58 +1000

    Update LuaUI.hpp

--- app/src/main/cpp/LuaUI.hpp
+++ app/src/main/cpp/LuaUI.hpp
@@ -9,27 +9,29 @@ std::string LuaUI = R"(
 
 -- Instances:
 
-local MobileBlox = Instance.new('ScreenGui')
-local Main = Instance.new('Frame')
-local TextBox = Instance.new('TextBox')
-local Clear = Instance.new('TextButton')
-local Execute = Instance.new('TextButton')
+local MobileBlox = Instance.new("ScreenGui")
+local Main = Instance.new("Frame")
+local TextBox = Instance.new("TextBox")
+local Clear = Instance.new("TextButton")
+local Execute = Instance.new("TextButton")
+local ImageLabel = Instance.new("ImageLabel")
+local TextLabel = Instance.new("TextLabel")
 
 --Properties:
 
 MobileBlox.Name = "MobileBlox"
-MobileBlox.Parent = game:WaitForChild('CoreGui')
+MobileBlox.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 
 Main.Name = "Main"
 Main.Parent = MobileBlox
 Main.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
-Main.Position = UDim2.new(0.318584085, 0, 0.197959185, 0)
-Main.Size = UDim2.new(0, 492, 0, 282)
+Main.Position = UDim2.new(0.199891418, 235, 0.120539807, 570)
+Main.Size = UDim2.new(0, 699, 0, 378)
 
 TextBox.Parent = Main
 TextBox.BackgroundColor3 = Color3.fromRGB(33, 33, 33)
-TextBox.Position = UDim2.new(0.0375582054, 0, 0.0874655992, 0)
-TextBox.Size = UDim2.new(0, 450, 0, 195)
+TextBox.Position = UDim2.new(0.0197888371, 0, 0.111275166, 0)
+TextBox.Size = UDim2.new(0, 671, 0, 263)
 TextBox.ClearTextOnFocus = false
 TextBox.Font = Enum.Font.Ubuntu
 TextBox.MultiLine = true
@@ -42,7 +44,7 @@ TextBox.TextYAlignment = Enum.TextYAlignment.Top
 Clear.Name = "Clear"
 Clear.Parent = Main
 Clear.BackgroundColor3 = Color3.fromRGB(144, 0, 0)
-Clear.Position = UDim2.new(0.544715464, 0, 0.822695017, 0)
+Clear.Position = UDim2.new(0.680623889, 0, 0.838568151, 0)
 Clear.Size = UDim2.new(0, 200, 0, 50)
 Clear.Font = Enum.Font.SourceSans
 Clear.Text = "Clear"
@@ -54,7 +56,7 @@ Clear.TextWrapped = true
 Execute.Name = "Execute"
 Execute.Parent = Main
 Execute.BackgroundColor3 = Color3.fromRGB(63, 190, 93)
-Execute.Position = UDim2.new(0.0365853645, 0, 0.822695017, 0)
+Execute.Position = UDim2.new(0.0365853794, 0, 0.838568151, 0)
 Execute.Size = UDim2.new(0, 200, 0, 50)
 Execute.Font = Enum.Font.SourceSans
 Execute.Text = "Execute"
@@ -63,72 +65,59 @@ Execute.TextScaled = true
 Execute.TextSize = 14.000
 Execute.TextWrapped = true
 
+ImageLabel.Parent = Main
+ImageLabel.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+ImageLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageLabel.BorderSizePixel = 0
+ImageLabel.Position = UDim2.new(0.00715307565, 0, 0.0057603945, 0)
+ImageLabel.Size = UDim2.new(0, 37, 0, 33)
+ImageLabel.Image = "http://www.roblox.com/asset/?id=12683173481"
+ImageLabel.ImageColor3 = Color3.fromRGB(252, 255, 255)
+
+TextLabel.Parent = Main
+TextLabel.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.BorderSizePixel = 0
+TextLabel.Position = UDim2.new(0.0901287571, 0, 0.00529100513, 0)
+TextLabel.Size = UDim2.new(0, 139, 0, 34)
+TextLabel.Font = Enum.Font.SourceSans
+TextLabel.Text = "Frost X Neo Excecutor BY GunyahVR"
+TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.TextSize = 14.000
+
 -- Scripts:
 
-local function BGWS_fake_script() -- Main.DragScript
-	local script = Instance.new('LocalScript', Main)
-
-	--Not made by me, check out this video: https://www.youtube.com/watch?v=z25nyNBG7Js&t=22s
-	--Put this inside of your Frame and configure the speed if you would like.
-	--Enjoy! Credits go to: https://www.youtube.com/watch?v=z25nyNBG7Js&t=22s
-
-	local UIS = game:GetService('UserInputService')
-	local frame = script.Parent
-	local dragToggle = nil
-	local dragSpeed = 0.25
-	local dragStart = nil
-	local startPos = nil
-
-	local function updateInput(input)
-		local delta = input.Position - dragStart
-		local position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X,
-			startPos.Y.Scale, startPos.Y.Offset + delta.Y)
-		game:GetService('TweenService'):Create(frame, TweenInfo.new(dragSpeed), {Position = position}):Play()
-	end
-
-	frame.InputBegan:Connect(function(input)
-		if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) then
-			dragToggle = true
-			dragStart = input.Position
-			startPos = frame.Position
-			input.Changed:Connect(function()
-				if input.UserInputState == Enum.UserInputState.End then
-					dragToggle = false
-				end
-			end)
-		end
-	end)
-
-	UIS.InputChanged:Connect(function(input)
-		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
-			if dragToggle then
-				updateInput(input)
-			end
-		end
-	end)
-
-end
-coroutine.wrap(BGWS_fake_script)()
-local function NBUT_fake_script() -- Clear.LocalScript
+local function CTPCQDE_fake_script() -- Clear.LocalScript 
 	local script = Instance.new('LocalScript', Clear)
 
-	local box = script.Parent.Parent.TextBox
-	local btn = script.Parent
-
-	btn.MouseButton1Click:Connect(function()
-		box.Text = "";
-	end)
+	coroutine.wrap(NBUT_fake_script)()
+	local function NBUT_fake_script() -- Clear.LocalScript
+		local script = Instance.new('LocalScript', Clear)
+	
+		local box = script.Parent.Parent.TextBox
+		local btn = script.Parent
+	
+		btn.MouseButton1Click:Connect(function()
+			box.Text = "";
+		end)
+	end
+	
+	
 end
-coroutine.wrap(NBUT_fake_script)()
-local function OBDPHLQ_fake_script() -- Execute.LocalScript
+coroutine.wrap(CTPCQDE_fake_script)()
+local function XTCHMDI_fake_script() -- Execute.LocalScript 
 	local script = Instance.new('LocalScript', Execute)
 
-	local box = script.Parent.Parent.TextBox
-	local btn = script.Parent
-
-	btn.MouseButton1Click:Connect(function()
-		loadstring(box.Text)()
-	end)
+	coroutine.wrap(NBUT_fake_script)()
+	local function OBDPHLQ_fake_script() -- Execute.LocalScript
+		local script = Instance.new('LocalScript', Execute)
+	
+		local box = script.Parent.Parent.TextBox
+		local btn = script.Parent
+	
+		btn.MouseButton1Click:Connect(function()
+			loadstring(box.Text)()
+		end)
+	end
 end
-coroutine.wrap(OBDPHLQ_fake_script)()
-)";
+coroutine.wrap(XTCHMDI_fake_script)()
